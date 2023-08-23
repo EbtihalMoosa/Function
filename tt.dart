9c966bb2c8eabb4917dd404e0c468b6b5a944a19
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-08-23 00:15:57 -0700

    Add files via upload
    Parameter and return

---
+++ tt.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+void main (){
+        //return type - fun name(param)
+         //مجموع رقمين
+//Example:
+sum (1,34); }
+void sum(int a, int b){
+print(a+b);
+}
+
+// () here is parameter
+//no return -no parameter
+void welcome (){
+  print("hello");
+}
+
+
+//return - no paramerter
+String getName(){
+  return "Ebtihal";
+}
+
+//no return -parameter
+void setName(String name){
+  print(name);
+}
+
+//return - paramerter
+int x=1; 
+int y=3;
+int number(int x, int y){
+  return x + y;
+}
+
+
+
+
